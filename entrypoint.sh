16a47fe99ee91b96625f7b1a7fa513ab8c7ef911
PJ Fanning <pjfanning@users.noreply.github.com>
2025-04-17 00:27:53 +0200

    refactor

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Start the script to create the DB and user
-/usr/config/configure-db.sh &
+/usr/config/configure-db.sh &> /usr/config/config.log &
 
 # Start SQL Server
 /opt/mssql/bin/sqlservr
--- setup.sql
+++ setup.sql
@@ -6,6 +6,7 @@ Enter custom T-SQL here that would run after SQL Server has started up.
 
 CREATE DATABASE docker;
 CREATE LOGIN docker WITH PASSWORD = 'Str0ngPassword';
+GO
 USE docker;
 CREATE user docker FOR LOGIN docker;
 GO
