4c136abb58c48e5a240feb0a506e4c31acc8f3fd
PJ Fanning <pjfanning@users.noreply.github.com>
2025-04-17 14:50:39 +0200

    assign permissions

--- setup.sql
+++ setup.sql
@@ -9,4 +9,7 @@ CREATE LOGIN docker WITH PASSWORD = 'Str0ngPassword';
 GO
 USE docker;
 CREATE user docker FOR LOGIN docker;
+ALTER ROLE db_datareader ADD MEMBER docker;
+ALTER ROLE db_datawriter ADD MEMBER docker;
+ALTER ROLE db_ddladmin ADD MEMBER docker;
 GO
